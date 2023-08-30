a243170932fd5f09dbfc971cbe7e00ebac6aa6ce
Darshan <darshanpanchal557@gmail.com>
2023-08-30 12:14:32 +0530

    Test Case 1

---
+++ Chandrayaan3Test.java
@@ -0,0 +1,32 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+
+public class Chandrayaan3Test {
+    @Test
+    public void testChandraan() {
+        Chandrayan3 chandryaan = new Chandrayan3("N",new int[]{0,0,0});
+
+        assertEquals("N", chandryaan.dir);
+        assertArrayEquals(new int[] { 0, 0, 0 }, chandryaan.coordinates);
+
+
+        Chandrayan3 obj = new Chandrayan3("N",new int[]{0,0,0});
+        char[] commands = { 'f', 'r', 'u', 'b', 'l' };
+
+        obj.handleMove(commands);
+
+        assertEquals("N", obj.dir);
+        assertArrayEquals(new int[] { 0, 1, -1 }, obj.coordinates);
+
+
+        Chandrayan3 obj1 = new Chandrayan3("N",new int[]{0,0,0});
+        char[] commands1 = { 'f', 'b' , 'b'};
+
+        obj1.handleMove(commands1);
+
+        assertEquals("N", obj1.dir);
+        assertArrayEquals(new int[] { 0, -1, 0 }, obj1.coordinates);
+    }
+}
